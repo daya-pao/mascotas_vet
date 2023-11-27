8085be709022254bd57214f778bc026fd975aed3
daya-pao <dayana06052002@gmail.com>
2023-11-27 15:55:21 -0500

    no tanto terminado

--- componentes/header.php
+++ componentes/header.php
@@ -1,10 +1,19 @@
-
+<?php 
+session_start();
+if (isset($_SESSION['nombreUsuario']) && isset($_SESSION['rolUsuario'])) {
+    $nombreUsuario = $_SESSION['nombreUsuario'];
+    $rolUsuario = $_SESSION['rolUsuario']; 
+    $rolnombre = ($rolUsuario == 1) ? 'User' : 'Admin';
+} else {
+    echo "error";
+}
+?>
 <header class="encabezado">
     <div class="perfil">
         <div class="border"><img src="../img/user_vector.png" class="user_ejemplo"></div>
         <div>
-          <p >nombre:</p>
-          <p>Rol:</p>
+          <p ><?php echo $_SESSION['nombreUsuario']?></p>
+          <p ><?php echo $_SESSION['rolUsuario'] == 1 ? 'admin' : 'user'?></p>
         </div>
     </div>
     <div class="nav_contenido">
